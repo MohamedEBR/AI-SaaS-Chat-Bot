f12587a95d646f7006de09a853681a17892a861f
MohamedEBR <110148669+MohamedEBR@users.noreply.github.com>
2024-03-02 09:12:36 -0500

    Final commit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,12 +10,12 @@ config();
 const app = express();
 
 //middlewares
-app.use(cors({origin:"http://localhost:5173", credentials: true}));
+app.use(
+  cors({ origin:"https://ai-mern-chatbot.netlify.app", credentials: true })
+);
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET as string));
 
-//remove in production
-app.use(morgan("dev"));
 
 app.use("/api/v1", appRouter);
 
